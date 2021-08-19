dc20537bd35f930bf8d14d3c90ded4337f1fd3c8
paulosgaf <paulosgaf@outlook.com>
2021-08-19 13:18:05 -0300

    16/08

--- routes.js
+++ routes.js
@@ -1,12 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const app = express();
 
 // const faker = require("faker");
 
 // let db = require("./db");
 
-//db.criarDB("minhaBaseDados");
+// db.criarDB("minhaBaseDados");
 
 //Especifica a pasta contendo arquivos estáticos. 
 //O nome 'public' não precisará ser colocado na rota 
@@ -16,7 +15,8 @@ router.use(express.static('public'));
 
 
 
-//Exemplode Rotas: 
+//************* Exemplode Rotas ************* 
+
 /*
 
 http://localhost:3030/css
@@ -25,46 +25,93 @@ http://localhost:3030/index.html
 
 / = http://localhost:3030/
 /about = http://localhost:3030/about
-/curriculo =  http://localhost:3030/cadastro
+/cadastro =  http://localhost:3030/cadastro
 
 */
-router.get('/',(req,res)=>{
+
+
+//Req é um objeto que recebe dados da requisição HTTP feita (request). Res permite enviar uma resposta ao navegador (Response)
+router.get('/',(req,res)=>{ //callback - funcao que trata dado evento GET
     res.render('pages/home');
 });
 
-router.get('/about',(req,res)=>{
+router.get('/about',(req,res)=>{ //callback - funcao que trata dado evento  GET
 
     res.render('pages/about');
 });
 
+router.get('/cadastro',(req,res)=>{ //callback - funcao que trata dado evento  GET
+
+    //a funcao render pode receber um pametro na forma de objeto literal
+    //no caso, ela irá receber um objeto com campo chamado users e com valor igual ao vetor users
+    res.render('pages/cadastro',{users:users}); 
+});
+
 router.post('/cadastro/remove',(req,res)=>{
-    let usuario={name: "wellington", email: "wwagner@virtual.ufc.br"};
-    /* for(let cont=1;cont<=6;cont++){
-        usuarios.push({name:faker.name.findName(),email: faker.internet.email(),avatar: faker.image.image()});
-    } */
+    //let item =req.body.id; //pega o valor passado através do parâmetro id e atribui a variável item. 
+    let name = req.body.name;
 
-    let result = db.inserirDado(usuario);
-    console.log(result);
-    //res.render('pages/insert',{result});
+    if(users.length==0){
+        console.log("Erro: Não há elemento a ser removido!");
+        return res.status(400).json({
+            status:'error',
+            error:`Removed element: ${name}`
+        });
+
+    } else {
+        for(let cont=0;cont<users.length;cont++){
+            if(users[cont].name==name){
+                users.splice(cont,1);
+                console.log("Elemento Removido: ",name);
+                return res.status(200).json({
+                    status:'sucess',
+                    data:users
+                });
+                //res.send(JSON.stringify({sucess:`Elemento removido com sucesso: ${name}`}));
+            } else if(cont==users.length-1){
+                console.log("Erro ao remover elemento: ",name);
+                return res.status(400).json({
+                    status:'error',
+                    error:`Removed element: ${name}`
+                });
+            }
+        }
+    }
+    
+    
+    //users.splice(item,1); //este método permite adicionar ou remover um item do vetor em uma dada posição. 
+    //res.render('pages/cadastro',{users:users});
+    //res.sendStatus(200); //envia mensagem 200 significando que as modificacoes foram ok
+    //res.send(JSON.stringify({sucess:`Elemento removido com sucesso: ${name}`}));
+    //console.log("Elemento Removido: ",name);
+    
 });
 
-router.get('/cadastro',(req,res)=>{
-  
-    res.render('pages/cadastro');
-});
 
-router.get('/cadastro/insert',(req,res)=>{
-    let usuario={name: "wellington", email: "wwagner@virtual.ufc.br"};
-    /* for(let cont=1;cont<=6;cont++){
-        usuarios.push({name:faker.name.findName(),email: faker.internet.email(),avatar: faker.image.image()});
-    } */
 
-    let result = db.inserirDado(usuario);
-    console.log(result);
-    //res.render('pages/insert',{result});
+router.post('/cadastro/update',(req,res)=>{
+    //substitui os valores armazenados no item do vetror dado por id, por valores fornecidos como parametro vindos do navegador.
+    //recebe dados do cliente na forma de um objeto JSON
+
+    users[req.body.id].name=req.body.name;
+    users[req.body.id].email=req.body.email;
+    users[req.body.id].address=req.body.address;
+    users[req.body.id].age=req.body.age;
+    users[req.body.id].height=req.body.height;
+    users[req.body.id].vote=req.body.vote;
+
+    console.log("Dados recebidos: ",req.body);//mostra no console do servidor os dados recebidos
+
+    res.sendStatus(200); //envia mensagem 200 significando que as modificacoes foram ok
 });
 
 router.get('/cadastro/list',(req,res)=>{
+    //Para fazer em casa: Como seria uma rotina para listar todos os itens cadastrados?
+
+});
+
+router.post('/cadastro/addUser',(req,res)=>{
+    //Para fazer em casa: Como seria uma rotina para listar todos os itens cadastrados?
 
 });
 
